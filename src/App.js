cca3953c27ac3a1ef6358bd0c1fa20ab37b0cddf
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-01-02 19:30:45 +0900

    scroll bar delete, bounce fixed

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ import FindPW from "./components/lost/findpw.components";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup1" element={<Signup1 />} />
-        <Route path="/signup2" element={<Signup2 />} />
-        <Route path="/find-id" element={<FindID />} />
-        <Route path="/find-pw" element={<FindPW />} />
-      </Routes>
+    <div id="main-container">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup1" element={<Signup1 />} />
+          <Route path="/signup2" element={<Signup2 />} />
+          <Route path="/find-id" element={<FindID />} />
+          <Route path="/find-pw" element={<FindPW />} />
+        </Routes>
+      </div>
     </div>
   );
 }
