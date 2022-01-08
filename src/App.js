2893e93cc026ef82bdc5820c65d28e6bdc07f526
[Github <[Github>
2022-01-08 12:59:56 +0900

    ad

--- src/App.js
+++ src/App.js
@@ -17,14 +17,24 @@ import EricsTodo from "./components/ericstodo/ericstodo.components";
 import ViewTemplate from "./components/viewtemplate/viewtemplate.components";
 import MainTemplateGrowth from "./components/maintemplategrowth/maintemplategrowth.components";
 import MoreTemplate from "./components/moretemplate/moretemplate.components";
-
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [realname, setRealname] = useState("");
-  const [phoneNum, setPhoneNum] = useState("01012345678");
-  const [emailAgree, setEmailAgree] = useState(1);
-  const [snsAgree, setSnsAgree] = useState(1);
+  const [emailAgree, setEmailAgree] = useState(0);
+  const [snsAgree, setSnsAgree] = useState(0);
+
+  const [service1, setService1] = useState(false);
+  const [service2, setService2] = useState(false);
+  const [service3, setService3] = useState(false);
+  const [service4, setService4] = useState(false);
+  const [service5, setService5] = useState(false);
+
+  const [iconStyle1, setIconStyle1] = useState("disabled");
+  const [iconStyle2, setIconStyle2] = useState("disabled");
+  const [iconStyle3, setIconStyle3] = useState("disabled");
+  const [iconStyle4, setIconStyle4] = useState("disabled");
+  const [iconStyle5, setIconStyle5] = useState("disabled");
 
   return (
     <div id="main-container">
@@ -38,13 +48,60 @@ function App() {
                 setEmailSignup={setEmail}
                 setPassword={setPassword}
                 setRealname={setRealname}
-                setPhoneNum={setPhoneNum}
+                setEmailAgree={setEmailAgree}
+                setSnsAgree={setSnsAgree}
+                service1={service1}
+                service2={service2}
+                service3={service3}
+                service4={service4}
+                service5={service5}
+                setService1={setService1}
+                setService2={setService2}
+                setService3={setService3}
+                setService4={setService4}
+                setService5={setService5}
+                iconStyle1={iconStyle1}
+                iconStyle2={iconStyle2}
+                iconStyle3={iconStyle3}
+                iconStyle4={iconStyle4}
+                iconStyle5={iconStyle5}
+                setIconStyle1={setIconStyle1}
+                setIconStyle2={setIconStyle2}
+                setIconStyle3={setIconStyle3}
+                setIconStyle4={setIconStyle4}
+                setIconStyle5={setIconStyle5}
+              />
+            }
+          />
+          <Route
+            path="/service"
+            element={
+              <Service
+                service1={service1}
+                service2={service2}
+                service3={service3}
+                service4={service4}
+                service5={service5}
+                setService1={setService1}
+                setService2={setService2}
+                setService3={setService3}
+                setService4={setService4}
+                setService5={setService5}
+                iconStyle1={iconStyle1}
+                iconStyle2={iconStyle2}
+                iconStyle3={iconStyle3}
+                iconStyle4={iconStyle4}
+                iconStyle5={iconStyle5}
+                setIconStyle1={setIconStyle1}
+                setIconStyle2={setIconStyle2}
+                setIconStyle3={setIconStyle3}
+                setIconStyle4={setIconStyle4}
+                setIconStyle5={setIconStyle5}
                 setEmailAgree={setEmailAgree}
                 setSnsAgree={setSnsAgree}
               />
             }
           />
-          <Route path="/service" element={<Service />} />
           <Route
             path="/signup2"
             element={
@@ -52,7 +109,6 @@ function App() {
                 email={email}
                 password={password}
                 realname={realname}
-                phoneNum={phoneNum}
                 emailAgree={emailAgree}
                 snsAgree={snsAgree}
               />
@@ -71,6 +127,7 @@ function App() {
         <Route path="/main/viewtemplate" element={<ViewTemplate />} />
         <Route path="/main/maintemplateroutine/moretemplate" element={<MoreTemplate />} />
         <Route path="/main/moretemplate" element={<MoreTemplate />} />
+
         </Routes>
       </div>
     </div>
