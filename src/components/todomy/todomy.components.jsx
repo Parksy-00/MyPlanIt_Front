1cc5c728becb13146e9e31d10574599deaf60c0c
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-02-04 00:55:49 +0900

    final final final fix

--- src/components/todomy/todomy.components.jsx
+++ src/components/todomy/todomy.components.jsx
@@ -222,7 +222,7 @@ function TodoMy() {
                   justifyContent: "center",
                   alignItem: "center",
                   textAlign: "right",
-                  marginTop: 9,
+                  marginTop: 0,
                   marginBottom: 0,
                   fontSize: "12px",
                   color: "#8977F7",
