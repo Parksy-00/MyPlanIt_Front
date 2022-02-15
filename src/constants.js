8c8cbb45c32b28416e770d51c13e7f7f13c27a24
Parksy-00 <parksy1202@naver.com>
2022-02-16 01:39:48 +0900

    constants 파일 생성

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const constants = {
+  TODO_COLORED:
+    "https://firebasestorage.googleapis.com/v0/b/single-life-manager.appspot.com/o/ceos%20test%2Ftodo3.png?alt=media&token=31e75be2-ab09-424c-9a43-745d4316beff",
+  TODO_UNCOLORED:
+    "https://firebasestorage.googleapis.com/v0/b/single-life-manager.appspot.com/o/ceos%20test%2Ftodoc.png?alt=media&token=3f20e0e8-1b61-4c72-a99b-a2d3fe24949e",
+  PLAN_COLORED:
+    "https://firebasestorage.googleapis.com/v0/b/single-life-manager.appspot.com/o/ceos%20test%2Ftodod.png?alt=media&token=55c5a1c6-a771-4619-ae0e-fa6a674683e3",
+  PLAN_UNCOLORED:
+    "https://firebasestorage.googleapis.com/v0/b/single-life-manager.appspot.com/o/ceos%20test%2Ftodof.png?alt=media&token=52344e83-350a-490d-83dc-144dd547bb52",
+  NO_PLAN_IMG: "/images/plan.png",
+  NO_TODO_IMG: "/images/my.png",
+  DO_TOMORROW: "/images/next.png",
+  DELETE: "/images/delete.png",
+  DETAIL_ICON: "/images/detail.png"
+};
+
+export default constants;
