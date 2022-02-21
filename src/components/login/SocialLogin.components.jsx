5c4af4fff2f76f0729b6d1877415462e3152b4d9
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-02-21 17:43:41 +0900

    회원가입 페이지 제작 + 시작 페이지 제작중

--- src/components/login/SocialLogin.components.jsx
+++ src/components/login/SocialLogin.components.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "antd";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import styled from "styled-components";
 
 function Login() {
   let navigate = useNavigate();
@@ -12,8 +12,8 @@ function Login() {
       .post(
         "https://myplanit.link/login",
         {
-          email: "asdf@naver.com",
-          password: "12341234",
+          email: "1@1.com",
+          password: "test1234",
         },
         {
           headers: {
@@ -26,7 +26,7 @@ function Login() {
         sessionStorage.setItem("token", response.data.token);
         sessionStorage.setItem("email", response.data.email);
         sessionStorage.setItem("password", response.data.password);
-        navigate("/main/todo");
+        navigate("/todo");
       })
       .catch((error) => {
         if (error.response.status === 400) {
@@ -41,22 +41,67 @@ function Login() {
 
   return (
     <div className="container">
-      <img
-        className="Logo"
-        src="/images/logo.png"
-        alt="Logo"
-        style={{ marginTop: "200px", width: "173px", marginBottom: "60px" }}
-      />
-      <Button>로그인</Button>
-      <Button style={{ height: "50px", width: "313px", marginTop: "20px"}}>구글로 시작하기</Button>
-      <img
-        src="/images/kakao_login.png"
-        alt="kakaoLogin"
-        style={{ height: "50px", marginTop: "20px" }}
-        onClick={() => alert("카카오 로그인")}
-      />
+      <BackButton>
+        <img
+          src="/images/cross.png"
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+      </BackButton>
+      <LoginText>
+        빠르고 간편한 로그인으로 마이플랜잇의 다양한 혜택을 누려 보세요!
+      </LoginText>
+      <LoginButtons>
+        <Button
+          onClick={() => {
+            attemptLogin();
+          }}
+        >
+          테스트 로그인
+        </Button>
+        <img
+          src="/images/google_login.png"
+          alt="googleLogin"
+          style={{ marginTop: "15px" }}
+          onClick={() => alert("구글 로그인")}
+        />
+
+        <img
+          src="/images/kakao_login.png"
+          alt="kakaoLogin"
+          style={{ marginTop: "15px" }}
+          onClick={() => alert("카카오 로그인")}
+        />
+      </LoginButtons>
     </div>
   );
 }
+
+const BackButton = styled.div`
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  left: 0px;
+  top: 30px;
+`;
+
+const LoginText = styled.span`
+  width: 258px;
+  height: 108px;
+  margin-right: 60px;
+  margin-top: 100px;
+  font-family: "PretendardMedium";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 36px;
+  letter-spacing: -0.01em;
+`;
+
+const LoginButtons = styled.span`
+  position: absolute;
+  bottom: 95px;
+`;
 
 export default Login;
--- src/components/login/login.components.jsx
+++ src/components/login/login.components.jsx
@@ -1,48 +1,7 @@
-import { useState } from "react";
-import "./login.components.css";
-import { Button } from "@nextui-org/react";
-import { Input, Switch } from "antd";
-import { Link } from "react-router-dom";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import styled from "styled-components";
 function Login() {
   let navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [autoLogin, setAutoLogin] = useState(false);
-
-  function attemptLogin() {
-    axios
-      .post(
-        "https://myplanit.link/login",
-        {
-          email: email,
-          password: password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
-        sessionStorage.removeItem("token");
-        sessionStorage.setItem("token", response.data.token);
-        sessionStorage.setItem("email", response.data.email);
-        sessionStorage.setItem("password", response.data.password);
-        navigate("/todo");
-      })
-      .catch((error) => {
-        if (error.response.status === 400) {
-          alert("존재하지 않는 이메일입니다");
-        } else if (error.response.status === 401) {
-          alert("비밀번호가 틀렸습니다");
-        } else {
-          alert("오류");
-        }
-      });
-  }
 
   return (
     <div className="container">
@@ -51,95 +10,23 @@ function Login() {
         src="/images/logo.png"
         style={{ marginTop: "200px", width: "173px", marginBottom: "101px" }}
       />
-      <Input
-        className="email-input"
-        size="large"
-        placeholder="이메일 입력"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        style={{
-          width: "327px",
-          marginBottom: "12px",
-          fontFamily: "PretendardRegular",
-          fontSize: "16px",
-          color: "black",
-        }}
-      />{" "}
-      <Input.Password
-        className="password-input"
-        size="large"
-        placeholder="비밀번호 입력"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        style={{
-          width: "327px",
-          marginBottom: "19px",
-          fontFamily: "PretendardRegular",
-          fontSize: "16px",
-          color: "#CECECE",
-        }}
-      />
-      <span className="switch">
-        <Switch 
-          size="small" 
-          checked={ autoLogin } 
-          onClick={() => setAutoLogin(!autoLogin)}
-        />
-        <p
-          style={{
-            marginTop: "0px",
-            marginBottom: "0px",
-            marginLeft: "10px",
-            color: autoLogin? "#8977f7": "#C4C4C4",
-            fontSize: "14.5px",
-            fontFamily: "PretendardRegular"
-          }}
-        >
-          자동 로그인
-        </p>
-      </span>
-      <button
-        onClick={() => {
-          attemptLogin();
-        }}
-        className="login-button"
-      >
-        로그인
-      </button>
-      <span
-        className="button-group"
-        style={{ marginTop: "8px", fontSize: "12px" }}
-      >
-        <p
-          style={{
-            marginLeft: "12px",
-            marginRight: "12px",
-            opacity: 0.3,
-            fontFamily: "PretendardRegular",
-            fontSize: "12px",
-            color: "#929292",
-          }}
-        >
-          아직 계정이 없으신가요?
-        </p>
-        <p>
-          <Link
-            style={{
-              color: "black",
-              fontFamily: "PretendardRegular",
-              fontSize: "12px",
-              color: "#000000",
-            }}
-            to="/signup"
-          >
-            회원가입하기
-          </Link>
-        </p>
-      </span>
+      <LogoText>어쩌구저쩌구, 마이플랜잇</LogoText>
     </div>
   );
 }
+
+const LogoText = styled.span`
+  position: absolute;
+  width: 201px;
+  height: 20px;
+  top: 250px;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 17px;
+  line-height: 20px;
+  text-align: center;
+  color: #929292;
+`;
 
 export default Login;
