18d5d58997a03026dbfd15e569c4841cc9a10df7
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-02-26 12:25:41 +0900

    admin page updated

--- src/components/admin/admin.components.jsx
+++ src/components/admin/admin.components.jsx
@@ -1,11 +1,62 @@
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Admin() {
+  let navigate = useNavigate();
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  function attemptLogin() {
+    axios
+      .post(
+        "https://myplanit.link/manager",
+        {
+          username: id,
+          password: pw,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        const data = response.data;
+        sessionStorage.setItem("access", data.django_token.access);
+        sessionStorage.setItem("refresh", data.django_token.refresh);
+        navigate("/todo");
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          alert("존재하지 않는 이메일입니다");
+        } else if (error.response.status === 401) {
+          alert("비밀번호가 틀렸습니다");
+        } else {
+          alert("오류");
+        }
+      });
+  }
+
   return (
     <Container>
-      <input style={{ marginBottom: "10px" }} placeholder="아이디 입력" />
-      <input style={{ marginBottom: "10px" }} placeholder="비밀번호 입력" />
-      <button>로그인</button>
+      <input
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+        style={{ marginBottom: "10px" }}
+        placeholder="아이디 입력"
+      />
+      <input
+        onChange={(e) => {
+          setPw(e.target.value);
+        }}
+        style={{ marginBottom: "10px" }}
+        placeholder="비밀번호 입력"
+      />
+      <button onClick={attemptLogin}>로그인</button>
     </Container>
   );
 }
