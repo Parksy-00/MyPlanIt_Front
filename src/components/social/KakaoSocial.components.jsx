75784bf1921b60632482dc914da7713103f1142a
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-03-15 21:54:29 +0900

    update kakao login

---
+++ src/components/social/KakaoSocial.components.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Loading } from "@nextui-org/react";
+import styled from "styled-components";
+
+function KakaoSocial() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const code = window.location.href.split("=")[1];
+    // console.log(code);
+    axios
+      .get(`https://myplanit.link/auth/kakao?code=${code}`)
+      .then((response) => {
+        const data = response.data;
+        const status = response.status;
+        sessionStorage.setItem("access", data.django_token.access);
+        sessionStorage.setItem("refresh", data.django_token.refresh);
+        navigate("/todo");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
+  return (
+    <LoadingIcon>
+      <Loading color="secondary" />
+    </LoadingIcon>
+  );
+}
+
+const LoadingIcon = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default KakaoSocial;
