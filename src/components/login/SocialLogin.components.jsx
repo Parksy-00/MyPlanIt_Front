bc6ff69d821dc89165aea2d96a6de7ab0b07e38a
Bokdol11859 <2019147551@yonsei.ac.kr>
2022-03-16 01:18:19 +0900

    google social login fixed

--- src/components/login/SocialLogin.components.jsx
+++ src/components/login/SocialLogin.components.jsx
@@ -12,13 +12,7 @@ function SocialLogin() {
   }
 
   function googleLogin() {
-    axios
-      .get(
-        `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile`
-      )
-      .then((response) => {
-        console.log(response);
-      });
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile`;
   }
 
   return (
--- src/components/social/GoogleSocial.components.jsx
+++ src/components/social/GoogleSocial.components.jsx
@@ -5,31 +5,21 @@ import { Loading } from "@nextui-org/react";
 import styled from "styled-components";
 
 function GoogleSocial() {
+  const navigate = useNavigate();
   useEffect(() => {
     const code = window.location.href.split("=")[1].slice(0, -6);
     console.log(code);
     axios
-      .post(
-        `https://oauth2.googleapis.com/token?code=${code}&clientid=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&clientsecret=${process.env.REACT_APP_GOOGLE_SECRET_KEY}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&grant_type=authorization_code`,
-        {
-          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-          client_secret: process.env.REACT_APP_GOOGLE_SECRET_KEY,
-          code: code,
-          grant_type: "authorization_code",
-        }
-      )
+      .get(`https://myplanit.link/auth/google?code=${code}`)
       .then((response) => {
-        console.log(response);
-        axios
-          .get(
-            `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${response.data.access_token}`
-          )
-          .then((response) => {
-            console.log(response);
-            // const data = response.data;
-            // sessionStorage.setItem("access", data.django_token.access);
-            // sessionStorage.setItem("refresh", data.django_token.refresh);
-          });
+        const data = response.data;
+        const status = response.status;
+        sessionStorage.setItem("access", data.django_token.access);
+        sessionStorage.setItem("refresh", data.django_token.refresh);
+        navigate("/todo");
+      })
+      .catch((error) => {
+        console.log(error.message);
       });
   });
 
