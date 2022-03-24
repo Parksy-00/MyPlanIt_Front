7a9bd109e7a4492de928a2bff13f94dee268d6a8
Parksy-00 <parksy1202@naver.com>
2022-03-24 16:25:30 +0900

    이미지 svg로 변경

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
 const constants = {
-  TODO_COLORED: "/images/todo_on@2x.png",
-  TODO_UNCOLORED: "/images/todo_off@2x.png",
-  PLAN_COLORED: "/images/planmarket_on@2x.png",
-  PLAN_UNCOLORED: "/images/planmarket_off@2x.png",
+  TODO_COLORED: "/images/todo_on.svg",
+  TODO_UNCOLORED: "/images/todo_off.svg",
+  PLAN_COLORED: "/images/planmarket_on.svg",
+  PLAN_UNCOLORED: "/images/planmarket_off.svg",
   NO_PLAN_IMG: "/images/plan.png",
   NO_TODO_IMG: "/images/my.png",
-  DO_TOMORROW: "/images/next.png",
-  DELETE: "/images/delete.png",
+  DO_TOMORROW: "/images/do_tomorrow.svg",
+  DELETE: "/images/delete.svg",
   DETAIL_ICON: "/images/detail.png",
 };
 
