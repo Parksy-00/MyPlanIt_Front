e7a51200225df0899b2e15771511c7e983290f33
Parksy-00 <parksy1202@naver.com>
2022-04-13 17:23:09 +0900

    EditFooter 수정

--- src/components/todo/EditFooter.components.jsx
+++ src/components/todo/EditFooter.components.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import constants from "../../constants";
-import styled from "styled-components";
+import * as Styled from "./EditFooter.style";
 
 function EditFooter({
   current,
@@ -47,7 +47,7 @@ function EditFooter({
           setUpdateMy(!updateMy);
         });
     }
-  }
+  };
 
   const advanceTodo = () => {
     for (let i = 0; i < delay.length; i++) {
@@ -82,56 +82,27 @@ function EditFooter({
         });
     }
   };
+  
   return (
-    <Footer>
-      {current == "PLANDETAIL" && (
-        <Button onClick={advanceTodo}>
+    <Styled.Footer>
+      {current != "MY" && (
+        <Styled.Button onClick={advanceTodo}>
           <img src={constants.DO_TOMORROW} height="19" />
-          <NavText>하루 앞당기기</NavText>
-        </Button>
+          <Styled.NavText>하루 앞당기기</Styled.NavText>
+        </Styled.Button>
       )}
-      <Button onClick={current == "MY"? delayMyTodo: delayTodo}>
+      <Styled.Button onClick={current == "MY" ? delayMyTodo : delayTodo}>
         <img src={constants.DO_TOMORROW} height="19" />
-        <NavText>
-          {current == "PLANDETAIL" ? "하루 미루기" : "내일하기"}
-        </NavText>
-      </Button>
+        <Styled.NavText>하루 미루기</Styled.NavText>
+      </Styled.Button>
       {current === "MY" && (
-        <Button onClick={deleteTodo}>
+        <Styled.Button onClick={deleteTodo}>
           <img src={constants.DELETE} height="19" />
-          <NavText>삭제하기</NavText>
-        </Button>
+          <Styled.NavText>삭제하기</Styled.NavText>
+        </Styled.Button>
       )}
-    </Footer>
+    </Styled.Footer>
   );
 }
 
 export default EditFooter;
-
-const Footer = styled.div`
-  right: 0;
-  left: 0;
-  height: 90px;
-  background-color: #8977f7;
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  justify-content: space-evenly;
-  padding: 0 20px;
-`;
-
-const Button = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 15px 30px 30px;
-  flex: 1;
-`;
-
-const NavText = styled.span`
-  font-family: "PretendardMedium";
-  font-size: 10px;
-  font-style: normal;
-  color: #ffffff;
-  margin-top: 8px;
-`;
---
+++ src/components/todo/EditFooter.style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Footer = styled.div`
+  right: 0;
+  left: 0;
+  height: 90px;
+  background-color: #8977f7;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 0 20px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 15px 30px 30px;
+  flex: 1;
+`;
+
+export const NavText = styled.span`
+  font-family: "PretendardMedium";
+  font-size: 10px;
+  font-style: normal;
+  color: #ffffff;
+  margin-top: 8px;
+`;
